428e1e4302602db7f235510d7df9e071f1826d9b
famikeeki <fami53977@gmail.com>
2025-06-14 01:43:50 +0900

    Add app.listen for Render deployment

--- functions/index.js
+++ functions/index.js
@@ -58,3 +58,8 @@ exports.appleSignIn = functions.https.onRequest(async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
