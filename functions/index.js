e80f2ef8f811a858add46492cf184b0b8e79132e
famikeeki <fami53977@gmail.com>
2025-06-14 17:39:19 +0900

    new

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB.HOST,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
